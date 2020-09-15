ecfb6517ab4afc8aedac2378226a58b2bacf4e67
clorin <cyrille.lorin@yahoo.fr>
2020-09-15 13:33:26 +0200

    auteur & libft.h

---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clorin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/14 13:06:04 by clorin            #+#    #+#             */
+/*   Updated: 2020/09/14 13:06:08 by clorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+void		ft_putchar(char c);
+void		ft_putstr(const char *str);
+size_t		ft_strlen(const char *str);
+void		*ft_memset(void *s, int c, size_t n);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
+
+#endif
