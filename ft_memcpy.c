a84f8407c82674715be589bab1c8c5db72cc91d3
clorin <cyrille.lorin@yahoo.fr>
2020-09-18 14:30:44 +0200

    debug

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -16,10 +16,8 @@ void		*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t		i;
 
-	if (!src || !dest || ft_strlen((const char *)dest) < n)
-		return ((void *)NULL);
 	i = 0;
-	while (((char *)src)[i] && i < n)
+	while (i < n)
 	{
 		((char *)dest)[i] = ((char *)src)[i];
 		i++;
