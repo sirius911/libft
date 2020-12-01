3b4d08befa1e52f655d8ece2002a120604b281a7
clorin <clorin@student.42.fr>
2020-12-01 09:20:20 +0100

    fix : add ft_strcmp() in libft.h

--- libft.h
+++ libft.h
@@ -41,6 +41,7 @@ int				ft_tolower(int ch);
 char			*ft_strchr(const char *str, int c);
 char			*ft_strrchr(const char *str, int c);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
+int				ft_strcmp(const char *s1, const char *s2);
 size_t			ft_strlcpy(char *dest, const char *src, size_t size);
 size_t			ft_strlcat(char *dest, const char *src, size_t size);
 char			*ft_strnstr(const char *s, const char *substr, size_t len);
