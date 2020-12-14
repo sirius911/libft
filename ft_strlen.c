13b93d98e6a7854e563152a54ec2c8c9001a15fa
clorin <clorin@student.42.fr>
2020-12-14 10:11:34 +0100

    feat : return 0 when str == NULL

--- ft_strlen.c
+++ ft_strlen.c
@@ -12,10 +12,12 @@
 
 #include "libft.h"
 
-size_t		ft_strlen(const char *str)
+size_t			ft_strlen(const char *str)
 {
-	int		i;
+	int			i;
 
+	if (!str)
+		return (0);
 	i = 0;
 	while (str[i])
 		i++;
